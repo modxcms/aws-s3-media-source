3102f334bd30d4e7cc69dfc7ed1470436523ae8f
matdave <mat@matdave.com>
2023-05-15 19:25:03 -0500

    adjusting fred plugin

--- core/components/awss3mediasource/elements/plugins/AWSS3Fred.php
+++ core/components/awss3mediasource/elements/plugins/AWSS3Fred.php
@@ -1,15 +1,4 @@
 <?php
-$corePath = $modx->getOption('awss3mediasource.core_path', null, $modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/awss3mediasource/');
-/** @var FredWebdam $FredWebdam */
-$FredWebdam = $modx->getService(
-    'awss3mediasource',
-    'AwsS3Fred',
-    $corePath . 'model/awss3mediasource/',
-    array(
-        'core_path' => $corePath
-    )
-);
-
 switch ($modx->event->name) {
     case 'FredOnElfinderRoots':
         $params = $modx->getOption('params', $scriptProperties);
@@ -17,13 +6,13 @@ switch ($modx->event->name) {
             return false;
         }
 
-        $mediaSourceIDs = $this->modx->getOption('mediaSource', $_GET, '');
+        $mediaSourceIDs = $modx->getOption('mediaSource', $_GET, '');
         $mediaSourceIDs = explode(',', $mediaSourceIDs);
         $mediaSourceIDs = array_map('trim', $mediaSourceIDs);
         $mediaSourceIDs = array_keys(array_flip($mediaSourceIDs));
         $mediaSourceIDs = array_filter($mediaSourceIDs);
 
-        $c = $this->modx->newQuery('modMediaSource');
+        $c = $modx->newQuery('modMediaSource');
         $where = [
             'class_key' => 'AwsS3MediaSource'
         ];
@@ -35,41 +24,60 @@ switch ($modx->event->name) {
         $c->where($where);
 
         /** @var \modFileMediaSource[] $mediaSources */
-        $mediaSources = $this->modx->getIterator('modMediaSource', $c);
-        include_once $FredWebdam->getOption('modelPath') . 'awss3mediasource/src/elFinderVolumeWebDam.php';
+        $mediaSources = $modx->getIterator('modMediaSource', $c);
         foreach ($mediaSources as $mediaSource) {
             $mediaSource->initialize();
             if (!$mediaSource->checkPolicy('list')) {
                 continue;
             }
 
-            $properties = $mediaSource->getProperties();
-            if (isset($properties['fred']) && ($properties['fred']['value'] === true)) {
+            $properties = $mediaSource->properties;
+            if (isset($properties['fred']) && ($properties['fred'] === true)) {
                 $readOnly = false;
-                if (isset($properties['fredReadOnly']) && ($properties['fredReadOnly']['value'] === true)) {
+                if (isset($properties['fredReadOnly']) && ($properties['fredReadOnly'] === true)) {
                     $readOnly = true;
                 }
-                $prefix = $properties['baseDir'];
-                $awsUrl = $properties['endpoint'];
-                $awsUrl = $prefix ? $awsUrl.'/'.$prefix : $awsUrl;
-                $adapter = new League\Flysystem\AwsS3V3\AwsS3V3Adapter(
-                    $mediaSource->driver,
-                    $mediaSource->bucket,
-                    $prefix
-                );
-                $filesystem = new League\Flysystem\Filesystem(
-                    $adapter,
-                    ['url' => $awsUrl]
-                );
+                $baseDir = $modx->getOption('baseDir', $properties, '');
+                $path = trim($baseDir, '/');
+                $endpoint = $modx->getOption('endpoint', $properties, '');
+                $bucket = $modx->getOption('bucket', $properties, '');
+                $awsUrl = $modx->getOption('url', $properties, '');
+                $awsUrl = $path ? $awsUrl.'/'.$path : $awsUrl;
 
-                $params->roots[] = [
-                    'id' => 'ms' . $mediaSource->id,
-                    'driver' => 'AwsS3Flysystem',
-                    'alias' => 'AwsS3',
-                    'filesystem' => $filesystem,
-                    'URL' => $awsUrl,
-                    'tmbURL' => 'self',
+                $config = [
+                    'version' => 'latest',
+                    'region' => $modx->getOption('region', $properties, ''),
+                    'credentials' => [
+                        'key' => $modx->getOption('key', $properties, ''),
+                        'secret' => $modx->getOption('secret_key', $properties, '')
+                    ]
                 ];
+
+                if (!empty($endpoint)) {
+                    $config['endpoint'] = $endpoint;
+                }
+                try {
+                    $client = new Aws\S3\S3Client($config);
+                    if (!$client->doesBucketExist($bucket)) {
+                        $modx->log(modX::LOG_LEVEL_ERROR, '[AWSS3MediaSource] Bucket does not exist: ' . $bucket);
+                        continue;
+                    }
+                    $adapter = new League\Flysystem\AwsS3V3\AwsS3V3Adapter(
+                        new Aws\S3\S3Client($config),
+                        $bucket,
+                        $path
+                    );
+                    $params->roots[] = [
+                        'id' => 'ms' . $mediaSource->id,
+                        'driver' => 'AwsS3Flysystem',
+                        'alias' => 'AwsS3',
+                        'filesystem' => new League\Flysystem\Filesystem($adapter),
+                        'URL' => $awsUrl,
+                        'tmbURL' => 'self',
+                    ];
+                } catch (Exception $e) {
+                    $modx->log(modX::LOG_LEVEL_ERROR, '[AWSS3MediaSource] Error: ' . $e->getMessage());
+                }
             }
         }
         break;
